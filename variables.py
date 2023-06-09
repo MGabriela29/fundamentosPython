0e845f7d980abdb7d50a76241f0859bdc53c76b8
Gabriela Hernandez <gabsram2975@gmail.com>
2023-06-09 13:24:14 -0600

    02 Variables

---
+++ variables.py
@@ -0,0 +1,13 @@
+#Python es no Tipado
+
+name = "Gabriela Hernandez"
+age = 39
+height = 1.57
+is_student = False
+children = []
+
+print(type (name))
+print(type(age))
+print(type(height))
+print(type(is_student))
+print(type(children))
