dbda6a89caf5459b0422f753f3899adadfb47bb6
Gabriela Hernandez <gabsram2975@gmail.com>
2023-06-09 13:59:42 -0600

    04 Operadores

---
+++ operadores.py
@@ -0,0 +1,17 @@
+#Operadores Matemáticos
+# + - * %
+#/ división float
+#// División entera 
+#** Potencia
+print(2+2)
+print(2-2)
+print(2*2)
+print(2%2)
+print(10 /3)
+print(10 // 3)
+print (3 ** 3)
+
+#Operadores para Strings
+# + (concatena) * (replica)
+print("Hola"+" Mundo!")
+print("Hola \n" *4)
