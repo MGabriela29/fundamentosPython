005bc0be5a62c95a7358c9150d68dad724b1f68e
Gabriela Hernandez <gabsram2975@gmail.com>
2023-06-12 09:53:37 -0600

    05 Operadores Logicos y Booleanos

---
+++ operadores_2.py
@@ -0,0 +1,22 @@
+#Operadoires booleanos 
+#> >=
+print(10>10)
+print(10>=10)
+#< <=
+print(10<10)
+print(10<=10)
+#==
+print(10==10)
+#!=
+print(10!=10)
+
+# Operadores Lógicos
+# and (True if all True)
+print(True and True)
+print(True and False)
+# or (True if al  least one True)
+print(True or True)
+print(True or False)
+# not (not True)
+print(not True)
+print(not False)
