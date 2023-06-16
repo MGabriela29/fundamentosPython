0fe337d044e15d1c4efd30f850f133a8bae4d718
Gabriela Hernandez <gabsram2975@gmail.com>
2023-06-16 13:25:49 -0600

    12 Funciones

---
+++ funciones.py
@@ -0,0 +1,33 @@
+# Funciones 
+# def nmae_function(params):
+#   code
+
+# Sin parametros y sin retorno
+def saluda ():
+    print("Hola a Todos!")
+
+saluda()
+
+# con parametros, sin retorno
+def duplica(num):
+    print(num * 2)
+
+duplica(5)
+
+def suma(num1, num2):
+    print(f"La suma de los numeros es: {num1 +num2}")
+
+suma(23, 45)
+
+# parametros opcionales, con retorno
+def get_lista(al_1 = "Jose", al_2="Darlen"):
+    return [al_1, al_2]
+
+my_list = get_lista()
+print (my_list)
+my_list = get_lista("Peter")
+print (my_list)
+my_list = get_lista("Peter Parker", "Tony Stark")
+print (my_list)
+my_list = get_lista(al_2="Peter Parker", al_1="Tony Stark")
+print (my_list)
